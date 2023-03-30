4db62c1249b4f486d92b33955129d67c4c5a2720
brunoangelovito <bruno.as1@hotmail.com>
2023-03-30 11:08:05 -0300

    sequencias

---
+++ atividade3_sequencia.py
@@ -0,0 +1,29 @@
+# a) 1, 3, 5, 7, __
+print("a) 1, 3, 5, 7, __")
+print("a sequencia é formada pela soma do nº anterior com 2 (1+2= 3, 3+2 = 5, 5+2 = 7...)\npróximo número da sequencia é:", 7+2)
+print()
+
+# b) 2, 4, 8, 16, 32, 64, __
+print("\nb) 2, 4, 8, 16, 32, 64, __")
+print("a sequencia é formada pelo dobro do nº anterior.\npróximo número da sequencia é:", 64*2)
+print()
+
+# c) 0, 1, 4, 9, 16, 25, 36, __
+print("\nc) 0, 1, 4, 9, 16, 25, 36, __")
+print("sequencia de nº naturais, cuja a raiz quadrada é também um nº positivo( √1 = 1, √4 = 2, √9 = 3, √16 = 4, √25 = 5, √36 =6, √49 = 7)\npróximo número da sequencia é:",7*7)
+print()
+
+# d) 4, 16, 36, 64, __
+print("\nd) 4, 16, 36, 64, __")
+print("quadrado dos nº pares em ordem crescente (2x2, 4x4, 6x6...)\npróximo número da sequencia é:",8**2)
+print()
+
+# e) 1,1,2,3,5,8, __
+print("\ne)1 ,1 ,2 ,3 ,5 ,8 ,__")
+fib = [1 ,1 ,2 ,3 ,5 ,8]
+print("sequencia de fibonacci (o nº novo é a soma dos dois nº anteriores)\npróximo número da sequencia é:", fib[-1] + fib[-2])
+print()
+
+# f)2 ,10 ,12 ,16 ,17 ,18 ,19, __
+print("\nf)2 ,10 ,12 ,16 ,17 ,18 ,19")
+print("nesta sequencia todos os numeros começam com a letra 'D', portanto o próximo é duzentos", 200)
